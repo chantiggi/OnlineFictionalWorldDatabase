b1656d0162b05f6e4133cdadb85b77da00bc6dc5
Chantel Higginbotham <34529538+chantiggi@users.noreply.github.com>
2019-03-09 17:28:42 -0500

    Updated credentials in dbcon.js file

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_gendrond',
-    password        : '1201',
-    database        : 'cs340_gendrond'
+    user            : 'cs361_higginbc',
+    password        : '5000',
+    database        : 'cs361_higginbc'
 });
 module.exports.pool = pool;
