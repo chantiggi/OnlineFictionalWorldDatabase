5649e1a1435541fcc0e2afeca9570661893dee89
Chantel Higginbotham <34529538+chantiggi@users.noreply.github.com>
2019-03-09 15:54:42 -0500

    Added sign_up.handlebars file and added function to sign_up.js file to insert user data into User table of database

--- public/sign_up.js
+++ public/sign_up.js
@@ -3,6 +3,24 @@ module.exports = function(){
     var router = express.Router();
   
     // Functions Here
+
+    //function to add a new user to the database using input provided
+    router.post('/', function(req, res){
+      var mysql = req.app.get('mysql');
+      var sql = "INSERT INTO `User`(`username`, `password`, `firstName`, `lastName`, `email`, `userType`) "
+      + "VALUES (?, ?, ?, ?, ?, ?)";
+      var inserts = [req.body.username, req.body.password, req.body.firstName, req.body.lastName, req.body.email, req.body.userType];
+      sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+        if(error){
+          console.log(JSON.stringify(error));
+          res.write(JSON.stringify(error));
+          res.end();
+        }
+        else{
+          res.redirect('/dashboard');
+        }
+      })
+    });
   
     return router;
   }();
