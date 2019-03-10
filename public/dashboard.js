7f70d2c73d31adb30504476edac448ac9813efa6
Jill A <jmarlo.ackerman@gmail.com>
2019-03-09 17:21:08 -0700

    dashboard functionality/display universes per author

--- public/dashboard.js
+++ public/dashboard.js
@@ -2,7 +2,39 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
   
-    // Functions Here
+
+    function getUniverses(res, mysql, id, context, complete){
+      var query = "SELECT Universe.univName AS Name, Universe.univDescription AS Description, User.userID AS id FROM User, au.UserID, au.UniverseID FROM AuthorUniverses au " +
+          "LEFT JOIN User ON au.UserID=User.UserID " +
+          "LEFT JOIN Universe ON au.UniverseID=Universe.UniverseID WHERE User.UserID = ?";
+      var inserts = [id];
+  
+      mysql.pool.query(query, inserts, function(error, results, fields){
+          if(error){
+              console.log(error);
+              res.write(JSON.stringify(error));
+              res.end();
+          }
+          context.dashboard = results;
+          complete();
+      });
+  }
+
+    router.get('/', function(req, res){
+      var params = {};
+  
+      var callbackCount = 0;
+      var context = {};
+      var mysql = req.app.get('mysql');
+      getUniverses(res, mysql, context, complete);
+      function complete(){
+          callbackCount++;
+          if(callbackCount >= 1){
+              res.render('dashboard', context);
+          }
+  
+      }
+  });
   
     return router;
   }();
